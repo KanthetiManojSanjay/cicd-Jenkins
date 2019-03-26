ad84adfe929860b0c8e71ff1c013606d8061299b
Sanjay <kanthetisanjay7@gmail.com>
2019-03-26 13:09:49 +0530

    Inital Commit

---
+++ src/test/java/com/techprimers/jenkins/CicdJenkinsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.techprimers.jenkins;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CicdJenkinsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
